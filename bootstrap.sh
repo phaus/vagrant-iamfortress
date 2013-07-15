569476f15aabe9a6106344c0dffed277ed6ba52f
Philipp Haußleiter <philipp@haussleiter.de>
2013-07-15 10:58:53 +0200

    fixing issue #1 Download of fortress still is not finished, when bootstrap file continues setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,11 +6,9 @@ DIR=/root
 echo "DIR: $DIR"
 apt-get update --fix-missing
 #apt-get upgrade -y
-apt-get install -q -y openjdk-7-jdk curl unzip
+apt-get install -q -y openjdk-7-jdk wget unzip
 
-curl -o $DIR/fortressBuilder-Debian-Silver-x86-64-1.0-RC27.zip http://iamfortress.org/sites/default/files/fortressBuilder-Debian-Silver-x86-64-1.0-RC27.zip&nid=99
-echo "Wating for finishing Download..."
-sleep 5
+wget --output-document=$DIR/fortressBuilder-Debian-Silver-x86-64-1.0-RC27.zip "http://iamfortress.org/sites/all/modules/pubdlcnt/pubdlcnt.php?file=http://iamfortress.org/sites/default/files/fortressBuilder-Debian-Silver-x86-64-1.0-RC27.zip&nid=99"
 unzip $DIR/fortressBuilder-Debian-Silver-x86-64-1.0-RC27.zip -d /opt/fortress
 echo "FORTRESS_HOME=/opt/fortress" >> $DIR/.profile
 rm $DIR/fortressBuilder-Debian-Silver-x86-64-1.0-RC27.zip
@@ -18,6 +16,6 @@ source $DIR/.profile
 cd $FORTRESS_HOME
 chmod a+x -Rf *
 cp /vagrant/build.properties $FORTRESS_HOME/build.properties
-echo "sudo.pw=password" >> $FORTRESS_HOME/vagrant/build.properties
+echo "sudo.pw=password" >> $FORTRESS_HOME/build.properties
 cp /vagrant/b.sh $FORTRESS_HOME/b.sh
-b.sh dist
+./b.sh dist
